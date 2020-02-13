1dfddee65d93eecc003928a58da58ad370eee805
Liam Pulles <me@liampulles.com>
2020-02-13 17:26:25 +0200

    Release v0.0.1

--- pkg/meta/contants.go
+++ pkg/meta/contants.go
@@ -8,7 +8,7 @@ const (
 	// ProgramName is the name of this program.
 	ProgramName = "Cabiria"
 	// ProgramVersion is the version of this module
-	ProgramVersion = "v0.0.0"
+	ProgramVersion = "v0.0.1"
 	// ProgramURL is the homepage for this program
 	ProgramURL = "https://github.com/liampulles/cabiria"
 )
